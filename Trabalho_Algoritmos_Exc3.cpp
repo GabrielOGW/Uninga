6a35b7428f36218e89827047529da9707add9514
Gabriel Ogassawara de Souza <77122634+GabrielOGW@users.noreply.github.com>
2021-05-26 15:01:49 -0300

    Update Trabalho_Algoritmos_Exc3.cpp

--- Trabalho_Algoritmos_Exc3.cpp
+++ Trabalho_Algoritmos_Exc3.cpp
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+/*Foi feita uma estatística em algumas cidades brasileiras para coletar dados sobre acidentes de trânsito.
+Foram obtidos os seguintes dados: código da cidade, número de veículos de passeio e número de acidentes de trânsito com vítimas. 
+Deseja-se saber: a quantidade de cidades pesquisadas;
+Qual o maior e o menor índice de acidentes de trânsito;
+Qual a média de veículos nas cidades pesquisadas;
+Qual a média de acidentes de trânsito nas cidades com menos de 2.000 veículos de passeio.
+Para encerrar a pesquisa, o usuário deverá digitar o valor zero para o código da cidade. */
+
+
 main(){
 //variaveis:
     int somaVeiculos = 0, somaAcidentes = 0, contCidade = 0, contCidadePequena = 0, maior = 0, menor = 5000;
@@ -16,7 +25,7 @@ main(){
         scanf("%d",&veiculos);
         printf("Quantos acidentes de transito, com vitimas houve: ");
         scanf("%d",&acidentes);
-//Enquanto pega os dados das cidades vai somando em variaveis que serão usadas dps
+//Enquanto pega os dados das cidades vai somando em variaveis que serão usadas depois
         contCidade++;
         somaVeiculos = somaVeiculos + veiculos;
         if(acidentes > maior){
@@ -33,7 +42,7 @@ main(){
 //calcula a media de veiculos, e media de acidentes em cidades menores
     mediaAcidentes = somaAcidentes/contCidadePequena;
     mediaVeiculos = somaVeiculos / contCidade;
-    //apresenta na tela as informações solicitadas
+//apresenta na tela as informações solicitadas
     printf("\nA quantidade de cidades pesquisadas foram: %d cidades", contCidade);
     printf("\nA cidade com maior número de acidentes, houve: %d acidentes", maior);
     printf("\nA cidade com menor número de acidentes, houve: %d acidentes", menor);
