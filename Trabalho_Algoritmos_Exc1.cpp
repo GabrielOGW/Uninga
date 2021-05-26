7180ec584caf3c64ee877cc88b4c6027eb0d5f72
Gabriel Ogassawara de Souza <77122634+GabrielOGW@users.noreply.github.com>
2021-05-26 14:55:32 -0300

    Update Trabalho_Algoritmos_Exc1.cpp

--- Trabalho_Algoritmos_Exc1.cpp
+++ Trabalho_Algoritmos_Exc1.cpp
@@ -4,11 +4,15 @@
 digitado pelo usuário. */
 
 main(){
+    //Define variaveis que serão usadas
     int contador, valordesejado;
+    //Imprime e pergunta ao usuário do desejado pelo enunciado
     printf("Ate que valor voce deseja ver os numeros pares: ");
     scanf("%d",&valordesejado);
+    //ciclo de repetição utilizando o for para imprimir até o valor desejado
     for(contador = 0; contador < valordesejado; contador += 2){
         printf("%d\n",contador);
     }
+    //Informa o usuário que o programa acabou
     printf("\nFim!\n");
 }
