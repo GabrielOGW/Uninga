c8a2c9503553624ea60fac0ca13ccd61c8bb9921
Gabriel Ogassawara de Souza <77122634+GabrielOGW@users.noreply.github.com>
2021-05-26 14:53:13 -0300

    Update Trabalho_Algoritmos_Exc1.cpp

--- Trabalho_Algoritmos_Exc1.cpp
+++ Trabalho_Algoritmos_Exc1.cpp
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+/*  Faça um programa em C para mostrar todos os números pares entre o número 1 e um número
+digitado pelo usuário. */
+
 main(){
     int contador, valordesejado;
     printf("Ate que valor voce deseja ver os numeros pares: ");
