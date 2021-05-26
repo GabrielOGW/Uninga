b2849df48e0da02d9757eddfa24c621fb917b164
Gabriel Ogassawara de Souza <77122634+GabrielOGW@users.noreply.github.com>
2021-05-26 14:58:29 -0300

    Update Trabalho_Algoritmos_Exc2.cpp

--- Trabalho_Algoritmos_Exc2.cpp
+++ Trabalho_Algoritmos_Exc2.cpp
@@ -1,11 +1,20 @@
 #include <stdio.h>
 
+/*  Tem-se um conjunto de dados contendo a altura e o sexo (M ou F) de 15 pessoas.
+Faça um programa em C que calcule e mostre: 
+A maior e a menor altura do grupo;
+A média de altura das mulheres;
+O número de homens;
+O sexo da pessoa mais alta.  */
+
 main(){
+    //Define as variaveis
     float altura, mediaAltura, maiorAltura = 0, menorAltura = 3, somaAltura, somaM;
     char sexo;
     int cont, quantidadeM, quantidadeF;
     cont = 1;
     quantidadeM = 0;
+    //Ciclo de repetição
     while(cont <= 15){
         printf("Informe a altura da pessoa: ");
         scanf("%f",&altura);
@@ -21,6 +30,6 @@ main(){
         } else if(altura < menorAltura){
             menorAltura = altura;
         }
-        
+        //ainda está inacabado.
     }
 }
