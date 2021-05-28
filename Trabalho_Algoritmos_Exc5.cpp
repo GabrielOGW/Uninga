429bfee4a2ba1ce31c759dd95040dfa251d44e2b
Gabriel Ogassawara de Souza <77122634+GabrielOGW@users.noreply.github.com>
2021-05-28 08:33:01 -0300

    Update Trabalho_Algoritmos_Exc5.cpp

--- Trabalho_Algoritmos_Exc5.cpp
+++ Trabalho_Algoritmos_Exc5.cpp
@@ -13,8 +13,7 @@ main(){
     int menores = 0, jogador, times = 5, i = 0, j = 0;
     int idade, mediaIdade, pesoAcima, totalIdade;
     float peso, altura, mediaAltura, porcentagemPeso, totalAltura;
-/*
-    Apresentando erros pois não gera o resultado esperado, buscar como arruma-lo e alterar
+
     for(jogador = 1; jogador < 12; jogador++){
         for(i = 1; i < 6; i++){
             printf("Time %d\nJogador %d\nDigite a idade do jogador: ", i, jogador);
@@ -31,13 +30,13 @@ main(){
             if(peso > 80.0){
                 pesoAcima++;
             }
-        mediaAltura = totalAltura / jogadores;
-        porcentagemPeso = 100* pesoAcima / jogadores;
-        } // precisa terminar ainda falta coisa, está dando erro ao rodar
+        mediaAltura = totalAltura / jogador;
+        porcentagemPeso = 100* pesoAcima / jogador;
+        } //precisa terminar ainda falta coisa, está dando erro ao rodar
     mediaIdade = totalIdade / 11;
     }
     printf("\nA idade media dos jogadores é: %.d", mediaIdade);
-    printf("\nA altura media dos jogadores é: %.2f", mediaAltura);
+    /*printf("\nA altura media dos jogadores é: %.2f", mediaAltura);
     printf("\nA porcentagem de jogadores acima de 80 kgs: %.2f", porcentagemPeso);
     printf("\nA quantidade de jogadores menores de 18 anos é: %d\n", menores);*/
 }
