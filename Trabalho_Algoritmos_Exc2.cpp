f63001c0dcc8913710ee588a12c4637f2d9e5b72
Gabriel Ogassawara de Souza <77122634+GabrielOGW@users.noreply.github.com>
2021-07-26 19:16:50 -0300

    Update Trabalho_Algoritmos_Exc2.cpp

--- Trabalho_Algoritmos_Exc2.cpp
+++ Trabalho_Algoritmos_Exc2.cpp
@@ -9,27 +9,48 @@ O sexo da pessoa mais alta.  */
 
 main(){
     //Define as variaveis
-    float altura, mediaAltura, maiorAltura = 0, menorAltura = 3, somaAltura, somaM;
-    char sexo;
-    int cont, quantidadeM, quantidadeF;
-    cont = 1;
-    quantidadeM = 0;
-    //Ciclo de repetição
-    while(cont <= 15){
-        printf("Informe a altura da pessoa: ");
-        scanf("%f",&altura);
-        printf("Informe o sexo da pessoa(m ou f): ");
+    float altura, somaAltura, mediaAltura, maiorAltura = 0, menorAltura = 3;
+    int contM = 0, contF = 0, pessoa = 1;
+    char sexo, sexoMaior;
+    //inicio da repetição e coleta de informações
+    do{
+        printf("Qual sexo da pessoa da %dª pessoa do grupo(M ou F): ", pessoa);
         scanf("%s",&sexo);
-        if(sexo = "m" || "M"){
-            quantidadeM = quantidadeM + 1;
-        } else {
-            (somaM = somaM + altura);
+        
+        printf("A altura desta pessoa: ");
+        scanf("%f",&altura);
+        if(sexo == 'M'){
+            contM++;
+        } else if(sexo == 'F'){
+            contF++;
+            somaAltura = somaAltura + altura;
         }
+        
         if(altura > maiorAltura){
             maiorAltura = altura;
-        } else if(altura < menorAltura){
+        }
+        if(altura < menorAltura){
             menorAltura = altura;
         }
-        //ainda está inacabado.
+
+        if(sexo == 'M' && maiorAltura == altura){
+            sexoMaior = 'M';
+        }else if(sexo == 'F' && maiorAltura == altura){
+            sexoMaior = 'F';
+        }
+        pessoa++;
+    }while(pessoa <= 5);
+    //calcula media da altura
+    mediaAltura = somaAltura / contF;
+
+//Mostra os resultados solicitados
+    printf("\nA menor altura do grupo: %.2f\nA maior altura do grupo: %.2f", menorAltura, maiorAltura);
+    printf("\nA media da altura das mulheres é: %.2f", mediaAltura);
+    printf("\nO número de homens no grupo: %d", contM);
+    if(sexoMaior == 'F'){
+        printf("\nA maior pessoa do grupo é do sexo Feminino\n");
+    } else {
+        printf("\nA maior pessoa do grupo é do sexo Masculino\n");
     }
+    
 }
