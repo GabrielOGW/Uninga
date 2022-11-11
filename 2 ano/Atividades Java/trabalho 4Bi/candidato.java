962617db314c05ca4622b9784098a58f35957e34
GabrielOGW <gabrielogassawara2001@gmail.com>
2022-11-11 16:21:14 -0300

    continuação trabalho

--- 2 ano/Atividades Java/trabalho 4Bi/candidato.java
+++ 2 ano/Atividades Java/trabalho 4Bi/candidato.java
@@ -7,4 +7,17 @@ public class candidato {
     System.out.println("Candidato X: " + porcentagem); 
     return porcentagem;
   }
+
+  String verificaVencedor(int notaX, int notaY, int notaZ, int notaW) {
+    if (notaX > notaY && notaX > notaZ && notaX > notaW) {
+      return "candidato X venceu!";
+    } else if (notaY > notaX && notaY > notaZ && notaY > notaW) {
+      return "candidato Y venceu!";
+    } else if (notaZ > notaX && notaZ > notaY && notaZ > notaW) {
+      return "candidato Z venceu!";
+    } else if (notaW > notaX && notaW > notaZ && notaW > notaZ) {
+      return "candidato Z venceu!";
+    }
+    return "";
+  }
 }
--- 2 ano/Atividades Java/trabalho 4Bi/exec.java
+++ 2 ano/Atividades Java/trabalho 4Bi/exec.java
@@ -1,16 +1,34 @@
 import javax.swing.JOptionPane;
+import javax.swing.UIManager;
 
 public class exec {
   public static void main(String[] args) {
+    UIManager.put("OptionPane.cancelButtonText", "Cancelar");
+    UIManager.put("OptionPane.noButtonText", "Não");
+    UIManager.put("OptionPane.yesButtonText", "Sim");
+    UIManager.put("OptionPane.okButtonText", "Confirmar");
+
+    int confirm = 10;
     int quantidadeEleitores = 10;
     int qtdVotos = 0;
     int votoBranco = 0;
     int votoNulo = 0;
-    for (int i = 0; i < quantidadeEleitores; i++) {
-      int voto = Integer.parseInt(JOptionPane.showInputDialog(null,
-          "Informe seu voto:\n1 - candidato X\n2 - candidato Y\n3 - candidato Z\n4 - candidato W\n5 - Branco\n6 - Nulo",
-          "Urna Eletronica - Votação",
-          JOptionPane.QUESTION_MESSAGE));
+    int voto = 0;
+    while (confirm != JOptionPane.YES_OPTION) {
+      for (int i = 0; i < quantidadeEleitores; i++) {
+        voto = Integer.parseInt(JOptionPane.showInputDialog(null,
+            "Informe seu voto:\n1 - candidato X\n2 - candidato Y\n3 - candidato Z\n4 - candidato W\n5 - Branco\n6 - Nulo",
+            "Urna Eletronica - Votação - Eleitor: " + (i + 1),
+            JOptionPane.QUESTION_MESSAGE));
+
+        confirm = JOptionPane.showConfirmDialog(null, "Você quer confirmar seu voto no canditado " + voto,
+            "Urna Eletronica - Confirmar voto - Eleitor: " + (i + 1),
+            JOptionPane.YES_OPTION);
+        if (confirm != JOptionPane.YES_OPTION) {
+          i--;
+        }
+
+      }
 
       switch (voto) {
         case 1:
@@ -47,5 +65,7 @@ public class exec {
       qtdVotos++;
       System.out.println(qtdVotos);
     }
+  
   }
+  
 }
