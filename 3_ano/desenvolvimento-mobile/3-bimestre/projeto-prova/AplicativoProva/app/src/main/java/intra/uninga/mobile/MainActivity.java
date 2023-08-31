9ebd5787b2848adc1f6f45f4c90185f812e57250
GabrielOGW <gabrielogassawara2001@gmail.com>
2023-08-31 13:48:12 -0300

    trabalho de desenvolvimento mobile

---
+++ 3_ano/desenvolvimento-mobile/3-bimestre/projeto-prova/AplicativoProva/app/src/main/java/intra/uninga/mobile/MainActivity.java
@@ -0,0 +1,47 @@
+package intra.uninga.mobile;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+        private Button formButton;
+        private Button listButton;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
+
+                formButton = findViewById(R.id.formButton);
+                listButton = findViewById(R.id.listButton);
+
+                formButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                                openFormActivity();
+                        }
+                });
+
+                listButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                                openListActivity();
+                        }
+                });
+        }
+
+        private void openFormActivity() {
+                Intent intent = new Intent(this, FormActivity.class);
+                startActivity(intent);
+        }
+
+        private void openListActivity() {
+                Intent intent = new Intent(this, ListActivity.class);
+                startActivity(intent);
+        }
+}
